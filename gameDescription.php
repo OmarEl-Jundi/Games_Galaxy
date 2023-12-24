5f3bc3b766db607522fcb21b4e3807a956034bb7
OmarEl-Jundi <omar.eljundi@hotmail.com>
2023-12-24 14:37:51 +0200

    fixed add to cart in game description

--- gameDescription.php
+++ gameDescription.php
@@ -78,7 +78,7 @@ if (!isset($_GET['gameID'])) {
                                         <?php echo ($games['price'] == 0) ? 'Free!' : ('$' . $games['price']); ?>
                                     </div>
                                 </div>
-                                <button class="RemoveFromCartBtn" data-game-id="<?= $games['game_id'] ?>">
+                                <button class="RemoveFromCartBtn" data-game-id="<?= $gameID ?>">
                                     <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke="red" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-trash-2">
                                         <polyline points="3 6 5 6 21 6"></polyline>
                                         <path d="M16 6v-4a1 1 0 0 0-1-1H9a1 1 0 0 0-1 1v4"></path>
@@ -154,7 +154,7 @@ if (!isset($_GET['gameID'])) {
             <div class="gameDescription">
                 <div class="gameImage">
                     <div style="position: fixed; right: 1%" class="addToWhishListBtn" data-game-id="<?= $gameID ?>">
-                        <button style="position: static;" class="SaveToWishlistBtn" data-game-id="<?= $games['id'] ?>">
+                        <button style="position: static;" class="SaveToWishlistBtn" data-game-id="<?= $gameID ?>">
                             <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-save">
                                 <path d="M18 2H6a2 2 0 0 0-2 2v16l7-3 7 3V4a2 2 0 0 0-2-2z"></path>
                             </svg>
@@ -194,7 +194,7 @@ if (!isset($_GET['gameID'])) {
                     <div class="developerDescription"><?= $developerDescription ?></div>
                     <div class="gameButtons">
                         <div style="margin: 40px;" class="addToCartBtn" data-game-id="<?= $gameID ?>">
-                            <button class="CartBtn" data-game-id="<?= $games['id'] ?>">
+                            <button class="CartBtn" data-game-id="<?= $gameID ?>">
                                 <span class="IconContainer">
                                     <svg xmlns="http://www.w3.org/2000/svg" height="1em" viewBox="0 0 576 512" fill="rgb(17, 17, 17)" class="cart">
                                         <path d="M0 24C0 10.7 10.7 0 24 0H69.5c22 0 41.5 12.8 50.6 32h411c26.3 0 45.5 25 38.6 50.4l-41 152.3c-8.5 31.4-37 53.3-69.5 53.3H170.7l5.4 28.5c2.2 11.3 12.1 19.5 23.6 19.5H488c13.3 0 24 10.7 24 24s-10.7 24-24 24H199.7c-34.6 0-64.3-24.6-70.7-58.5L77.4 54.5c-.7-3.8-4-6.5-7.9-6.5H24C10.7 48 0 37.3 0 24zM128 464a48 48 0 1 1 96 0 48 48 0 1 1 -96 0zm336-48a48 48 0 1 1 0 96 48 48 0 1 1 0-96z"></path>
